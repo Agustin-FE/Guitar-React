575952522ea0f3fd29dd9a2980412dfe75c35c19
agustin <agustinporta2000@gmail.com>
2022-03-08 13:21:37 -0300

    serverlevantado

---
+++ api/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+//const user = require('./Users')
+//const product = require('./Product')
+//const carrito = require('./Carrito')
+
+//router.use('./Users', user)
+//router.use('./product', product)
+//router.use('./carrito', carrito)
+
+
+module.exports = router
