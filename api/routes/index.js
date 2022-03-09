fcbdc197d3a25f4ebb47de12ddd908b726f6b09b
andres-bonilla <x.andresbonilla@gmail.com>
2022-03-09 13:08:39 -0300

    Coneccion del home con el back

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 //const user = require('./Users')
-//const product = require('./Product')
+const product = require('./Products')
 //const carrito = require('./Carrito')
 
 //router.use('./Users', user)
-//router.use('./product', product)
+router.use('/product', product)
 //router.use('./carrito', carrito)
 
 
