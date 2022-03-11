a7aef6ba029356d67d9feb7ce767a0f5421649bd
agustin <agustinporta2000@gmail.com>
2022-03-11 14:20:29 -0300

    agus

--- api/models/Carrito.js
+++ api/models/Carrito.js
@@ -1,30 +1,10 @@
 const S = require('sequelize');
 const db = require('./db');
-const Products = require('./Products');
-const User = require("./Users");
 
 class Carrito extends S.Model {}
 
 Carrito.init({
-idProducts:{
-    type: S.INTEGER,
-    references:{
-      model: Products,
-      key: "id"
-    },
-    allowNull: false,
-},
-idUser:{
-  type: S.INTEGER,
-    references:{
-      model: User,
-      key: "id"
-    },
-    allowNull: false,
-},
-  cantidad:{
-    type: S.INTEGER,
-},
+
 }, { sequelize: db, modelName: 'carrito' });  
 
 
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const user = require('./users')
 const product = require('./products')
-//const carrito = require('./carrito')
+const carrito = require('./carrito')
 
 router.use('/users', user)
 router.use('/product', product)
-//router.use('/carrito', carrito)
+router.use('/carrito', carrito)
 
 
 module.exports = router
